63695d36bdc5f142f2a79cc233423abeec63d7d1
Josh Granberry <jdgranberry@gmail.com>
2015-12-29 17:05:25 -0600

    Added README

--- distillery.py
+++ distillery.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-''' When trying to answer a specific question I have, or find out more about a
-    particular subject, I like to use internet forums and their capability to
-    search posts within specific subforums to get an overview of my subject. I
-    find this user-submitted information to be an easily-digestible, relatable
-    way to learn, and by internally indentifying common themes in posts, I can
-    determine which pieces of information are most likely reliable or
-    significant.
 
-    That's probably more words than necessary to describe what humans have been
-    doing for 6 million years. Teaching each other about the world through
-    communication. The problem is sifting through and reading hundreds of posts
-    is a time-consuming process.
-
-    Reddit contains an enormous amount of searchable user-submitted information
-    on a multitude of subjects, and this program is an attempt to automate my
-    own "forum-assisted" learning process, with the goal of identifying common
-    themes after performing a Reddit search query. '''
 
 import argparse    # Command line argument parsing
 import praw        # Python Reddit API Wrapper
